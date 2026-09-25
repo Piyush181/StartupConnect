fe6acc6e4132eb23af076bd97cb96950f5539090
Piyush181 <piyush29122007@gmail.com>
2026-09-26 04:03:05 +0530

    sml change

--- database.py
+++ database.py
@@ -20,7 +20,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 MYSQL_CONFIG = {
     "host": os.getenv("STARTUP_CONNECT_DB_HOST", "localhost"),
     "user": os.getenv("STARTUP_CONNECT_DB_USER", "root"),
-    "password": os.getenv("STARTUP_CONNECT_DB_PASSWORD", "Soham@14"),
+    "password": os.getenv("STARTUP_CONNECT_DB_PASSWORD", "apIEHewSeq8"),
     "database": "startupconnect"
 }
 
